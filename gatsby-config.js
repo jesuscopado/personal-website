878d991dce0412eecd71ff529a9f61166272bf79
jesuscopado <jcr.22@hotmail.com>
2020-10-12 01:39:21 +0200

    theme colors updated (darker blue)

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,16 @@ module.exports = {
           basePath: "/",
           contentPath: "content/",
           showThemeLogo: false,
-          theme: "gh-inspired",
+          theme: {
+            'back': '#edf0f3',
+            'front': '#2b3137',
+            'lead': '#3971bf',
+            'lead-text': '#ffffff',
+            'line': '#ced9e3',
+            'skill-1': '#8b33ce',
+            'skill-2': '#fc8732',
+            'skill-3': '#2ebc50',
+          },
         },
       },
       {

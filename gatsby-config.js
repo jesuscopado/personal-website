f0a12a725a713faf85cbd0d1bb9601e7b1de3a06
jesuscopado <jcr.22@hotmail.com>
2020-10-11 18:43:09 +0200

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    siteMetadata: {
+      description: "Personal page of Jesús Copado",
+      locale: "en",
+      title: "Jesús Copado",
+    },
+    plugins: [
+      {
+        resolve: "@wkocjan/gatsby-theme-intro",
+        options: {
+          basePath: "/",
+          contentPath: "content/",
+          showThemeLogo: false,
+          theme: "gh-inspired",
+        },
+      },
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          icon: `content/images/JC_favicon.png`,
+        },
+      },
+    ],
+  }

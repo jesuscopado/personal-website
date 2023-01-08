0833d93a14ca26e9eb206a97efa9423e672a46dc
Jesús Copado <jcr.22@hotmail.com>
2023-01-08 15:21:39 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
           basePath: "/",
           contentPath: "content/",
           showThemeLogo: false,
-          theme: "bright-orange",
+          theme: "warm-red",
         },
       },
       {
